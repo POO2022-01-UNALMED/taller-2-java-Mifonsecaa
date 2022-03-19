47d205c18d781cfedcbaa7228ede01b81ac5c73d
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-03-19 16:31:45 -0500

    Create Asiento.java

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,18 @@
+package test;
+public class Asiento {
+	String color;
+	int precio;
+	int registro;
+
+	void cambiarColor(String color ) {
+		if (
+				color == "rojo" ||
+				color == "amarillo" || 
+				color == "verde" || 
+				color == "blanco" || 
+				color == "negro" 
+				) {			
+			this.color = color;
+		}
+	}
+}
