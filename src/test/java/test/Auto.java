248850b7a7f6e948b4b8fa7b581f0e2f734fa171
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-03-19 14:12:28 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,38 @@
+package test;
+import java.util.*;
+public class Auto {
+	
+	static int cantidadCreados;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String modelo;
+		
+		int precio;
+		
+		List asientos = new ArrayList();
+		
+		String marca;
+		
+		int registro;
+		
+	}
+	
+	int cantidadAsientos() {
+		int total = 0;
+		for(int i: asientos) {
+			
+			
+		}
+		
+		
+	}
+	
+	
+	String verificarIntegridad() {
+		
+		
+	}
+
+}
